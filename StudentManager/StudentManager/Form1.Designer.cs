77d2632416319098e770e943b144274df88ac6f8
MarkHuang1996 <MarkHuang1996@users.noreply.github.com>
2019-01-03 23:06:12 +0800

    menuStrip change1

--- StudentManager/StudentManager/Form1.Designer.cs
+++ StudentManager/StudentManager/Form1.Designer.cs
@@ -28,23 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.系统ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tmiModifyPwd = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
-            this.tmiClose = new System.Windows.Forms.ToolStripMenuItem();
-            this.学员管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsmiAddStudent = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.tsmiManageStudent = new System.Windows.Forms.ToolStripMenuItem();
-            this.成绩管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsmiQueryAndAnalysis = new System.Windows.Forms.ToolStripMenuItem();
+            this.systemsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pwdChangePToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.tsmiQuery = new System.Windows.Forms.ToolStripMenuItem();
-            this.考勤管理AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsmi_Card = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentManagerTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewStudentAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentsInfoIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scoureYToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.queryAndAnalysisQToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quickSearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.attendanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.attenceCheckToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,130 +56,109 @@
             // 
             // menuStrip1
             // 
-            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.系统ToolStripMenuItem,
-            this.学员管理ToolStripMenuItem,
-            this.成绩管理ToolStripMenuItem,
-            this.考勤管理AToolStripMenuItem});
+            this.systemsToolStripMenuItem,
+            this.studentManagerTToolStripMenuItem,
+            this.scoureYToolStripMenuItem,
+            this.attendanceToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
-            this.menuStrip1.Size = new System.Drawing.Size(1580, 35);
-            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Size = new System.Drawing.Size(1580, 33);
+            this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // 系统ToolStripMenuItem
+            // systemsToolStripMenuItem
             // 
-            this.系统ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tmiModifyPwd,
-            this.toolStripSeparator3,
-            this.tmiClose});
-            this.系统ToolStripMenuItem.Name = "系统ToolStripMenuItem";
-            this.系统ToolStripMenuItem.Size = new System.Drawing.Size(101, 29);
-            this.系统ToolStripMenuItem.Text = "System(&S)";
-            // 
-            // tmiModifyPwd
-            // 
-            this.tmiModifyPwd.Image = ((System.Drawing.Image)(resources.GetObject("tmiModifyPwd.Image")));
-            this.tmiModifyPwd.Name = "tmiModifyPwd";
-            this.tmiModifyPwd.Size = new System.Drawing.Size(252, 30);
-            this.tmiModifyPwd.Text = "Pwd Change(&C)";
-            // 
-            // toolStripSeparator3
-            // 
-            this.toolStripSeparator3.Name = "toolStripSeparator3";
-            this.toolStripSeparator3.Size = new System.Drawing.Size(249, 6);
-            // 
-            // tmiClose
-            // 
-            this.tmiClose.Image = ((System.Drawing.Image)(resources.GetObject("tmiClose.Image")));
-            this.tmiClose.Name = "tmiClose";
-            this.tmiClose.Size = new System.Drawing.Size(252, 30);
-            this.tmiClose.Text = "Exit(&E)";
-            // 
-            // 学员管理ToolStripMenuItem
-            // 
-            this.学员管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsmiAddStudent,
-            this.toolStripSeparator2,
-            this.tsmiManageStudent});
-            this.学员管理ToolStripMenuItem.Name = "学员管理ToolStripMenuItem";
-            this.学员管理ToolStripMenuItem.Size = new System.Drawing.Size(181, 29);
-            this.学员管理ToolStripMenuItem.Text = "StudentManager(&M)";
-            // 
-            // tsmiAddStudent
-            // 
-            this.tsmiAddStudent.Image = ((System.Drawing.Image)(resources.GetObject("tsmiAddStudent.Image")));
-            this.tsmiAddStudent.Name = "tsmiAddStudent";
-            this.tsmiAddStudent.Size = new System.Drawing.Size(252, 30);
-            this.tsmiAddStudent.Text = "New Student(&A)";
-            // 
-            // toolStripSeparator2
-            // 
-            this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(249, 6);
-            // 
-            // tsmiManageStudent
-            // 
-            this.tsmiManageStudent.Image = ((System.Drawing.Image)(resources.GetObject("tsmiManageStudent.Image")));
-            this.tsmiManageStudent.Name = "tsmiManageStudent";
-            this.tsmiManageStudent.Size = new System.Drawing.Size(252, 30);
-            this.tsmiManageStudent.Text = "Student Info(&Q)";
-            // 
-            // 成绩管理ToolStripMenuItem
-            // 
-            this.成绩管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsmiQueryAndAnalysis,
+            this.systemsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pwdChangePToolStripMenuItem,
             this.toolStripSeparator1,
-            this.tsmiQuery});
-            this.成绩管理ToolStripMenuItem.Name = "成绩管理ToolStripMenuItem";
-            this.成绩管理ToolStripMenuItem.Size = new System.Drawing.Size(154, 29);
-            this.成绩管理ToolStripMenuItem.Text = "ScoreManager(&J)";
-           
+            this.exitToolStripMenuItem});
+            this.systemsToolStripMenuItem.Name = "systemsToolStripMenuItem";
+            this.systemsToolStripMenuItem.Size = new System.Drawing.Size(101, 29);
+            this.systemsToolStripMenuItem.Text = "System(&S)";
             // 
-            // tsmiQueryAndAnalysis
+            // pwdChangePToolStripMenuItem
             // 
-            this.tsmiQueryAndAnalysis.Image = ((System.Drawing.Image)(resources.GetObject("tsmiQueryAndAnalysis.Image")));
-            this.tsmiQueryAndAnalysis.Name = "tsmiQueryAndAnalysis";
-            this.tsmiQueryAndAnalysis.Size = new System.Drawing.Size(272, 30);
-            this.tsmiQueryAndAnalysis.Text = "Query and Analysis(&Q)";
+            this.pwdChangePToolStripMenuItem.Name = "pwdChangePToolStripMenuItem";
+            this.pwdChangePToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.pwdChangePToolStripMenuItem.Text = "Pwd Change(&P)";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(269, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(249, 6);
             // 
-            // tsmiQuery
+            // studentManagerTToolStripMenuItem
             // 
-            this.tsmiQuery.Image = ((System.Drawing.Image)(resources.GetObject("tsmiQuery.Image")));
-            this.tsmiQuery.Name = "tsmiQuery";
-            this.tsmiQuery.Size = new System.Drawing.Size(272, 30);
-            this.tsmiQuery.Text = "Quick Query(&S)";
+            this.studentManagerTToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewStudentAToolStripMenuItem,
+            this.studentsInfoIToolStripMenuItem});
+            this.studentManagerTToolStripMenuItem.Name = "studentManagerTToolStripMenuItem";
+            this.studentManagerTToolStripMenuItem.Size = new System.Drawing.Size(174, 29);
+            this.studentManagerTToolStripMenuItem.Text = "StudentManager(&T)";
             // 
-            // 考勤管理AToolStripMenuItem
+            // addNewStudentAToolStripMenuItem
             // 
-            this.考勤管理AToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsmi_Card});
-            this.考勤管理AToolStripMenuItem.Name = "考勤管理AToolStripMenuItem";
-            this.考勤管理AToolStripMenuItem.Size = new System.Drawing.Size(136, 29);
-            this.考勤管理AToolStripMenuItem.Text = "Attendance(&A)";
+            this.addNewStudentAToolStripMenuItem.Name = "addNewStudentAToolStripMenuItem";
+            this.addNewStudentAToolStripMenuItem.Size = new System.Drawing.Size(258, 30);
+            this.addNewStudentAToolStripMenuItem.Text = "Add New Student(&A)";
             // 
-            // tsmi_Card
+            // studentsInfoIToolStripMenuItem
             // 
-            this.tsmi_Card.Image = ((System.Drawing.Image)(resources.GetObject("tsmi_Card.Image")));
-            this.tsmi_Card.Name = "tsmi_Card";
-            this.tsmi_Card.Size = new System.Drawing.Size(259, 30);
-            this.tsmi_Card.Text = "Attendance Check(&R)";
+            this.studentsInfoIToolStripMenuItem.Name = "studentsInfoIToolStripMenuItem";
+            this.studentsInfoIToolStripMenuItem.Size = new System.Drawing.Size(258, 30);
+            this.studentsInfoIToolStripMenuItem.Text = "Students Info(&I)";
+            // 
+            // scoureYToolStripMenuItem
+            // 
+            this.scoureYToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.queryAndAnalysisQToolStripMenuItem,
+            this.quickSearchToolStripMenuItem});
+            this.scoureYToolStripMenuItem.Name = "scoureYToolStripMenuItem";
+            this.scoureYToolStripMenuItem.Size = new System.Drawing.Size(98, 29);
+            this.scoureYToolStripMenuItem.Text = "Scoure(&Y)";
+            // 
+            // queryAndAnalysisQToolStripMenuItem
+            // 
+            this.queryAndAnalysisQToolStripMenuItem.Name = "queryAndAnalysisQToolStripMenuItem";
+            this.queryAndAnalysisQToolStripMenuItem.Size = new System.Drawing.Size(272, 30);
+            this.queryAndAnalysisQToolStripMenuItem.Text = "Query and Analysis(&Q)";
+            // 
+            // quickSearchToolStripMenuItem
+            // 
+            this.quickSearchToolStripMenuItem.Name = "quickSearchToolStripMenuItem";
+            this.quickSearchToolStripMenuItem.Size = new System.Drawing.Size(272, 30);
+            this.quickSearchToolStripMenuItem.Text = "Quick Search";
+            // 
+            // attendanceToolStripMenuItem
+            // 
+            this.attendanceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.attenceCheckToolStripMenuItem});
+            this.attendanceToolStripMenuItem.Name = "attendanceToolStripMenuItem";
+            this.attendanceToolStripMenuItem.Size = new System.Drawing.Size(136, 29);
+            this.attendanceToolStripMenuItem.Text = "Attendance(&A)";
+            // 
+            // attenceCheckToolStripMenuItem
+            // 
+            this.attenceCheckToolStripMenuItem.Name = "attenceCheckToolStripMenuItem";
+            this.attenceCheckToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.attenceCheckToolStripMenuItem.Text = "Attendance Check";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1580, 785);
-            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
@@ -196,20 +172,18 @@
 
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem 系统ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem tmiModifyPwd;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
-        private System.Windows.Forms.ToolStripMenuItem tmiClose;
-        private System.Windows.Forms.ToolStripMenuItem 学员管理ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem tsmiAddStudent;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
-        private System.Windows.Forms.ToolStripMenuItem tsmiManageStudent;
-        private System.Windows.Forms.ToolStripMenuItem 成绩管理ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem tsmiQueryAndAnalysis;
+        private System.Windows.Forms.ToolStripMenuItem systemsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pwdChangePToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
-        private System.Windows.Forms.ToolStripMenuItem tsmiQuery;
-        private System.Windows.Forms.ToolStripMenuItem 考勤管理AToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem tsmi_Card;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studentManagerTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewStudentAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studentsInfoIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scoureYToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem queryAndAnalysisQToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quickSearchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem attendanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem attenceCheckToolStripMenuItem;
     }
 }
 
